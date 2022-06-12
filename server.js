89604e4c4cb62e9be8d85cb733085405677b8093
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-06-12 18:52:25 -0400

    trying to fix heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const PORT = process.env.PORT;
+const PORT = 2121;
 require("dotenv").config();
 
 let db,
